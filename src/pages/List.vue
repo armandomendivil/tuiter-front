9a7a2181e5e82b0fa000e89e0f3e26c2b2aebf80
armando <armandomendivil.m@gmail.com>
2017-11-20 11:58:06 -0700

    Base project

---
+++ src/pages/List.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="card margin-card">
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'Index',
+  };
+</script>
+
+<style>
+  .margin-card {
+    margin-bottom: 20px;
+  }
+</style>
